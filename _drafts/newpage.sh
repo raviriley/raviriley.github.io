3d9dda66760dba37eda42fba70171b4fa26c8592
Ravi Riley <raviriley@gmail.com>
2020-03-08 16:24:23 -0700

    show _drafts

---
+++ _drafts/newpage.sh
@@ -0,0 +1,13 @@
+echo "page title: (ex: My Projects)"
+read title
+#echo "filename is lowercase and -'d title, i.e. my-portfolio"
+name=${title,,} #to lower case
+file=${name// /-}.md #replace spaces with - and adds .md
+echo "page permalink (/input/): "
+read permalink
+permalink=${permalink// /-} #replace spaces with -
+cp pageheader.txt $file
+echo created $file
+cat "$file" | sed -e "s/temptitle/$title/" -e "s/temp-permalink/$permalink/" > "$file"
+sleep 0.6s
+# Created by Ravi Riley (github.com/raviriley)
---
+++ _drafts/newpost.sh
@@ -0,0 +1,13 @@
+echo "post title: "
+read title
+name=${title,,} #to lower case
+name=${name// /-}.markdown #replace spaces with - and adds .markdown
+printf -v date '%(%Y-%m-%d)T' -1 #gets date and formats it to YYY-MM-DD
+file="$date-$name"
+cp postheader.txt $file
+echo created $file
+printf -v datetime '%(%Y-%m-%d %H:%M:%S)T' -1 #gets date and formats it to YYY-MM-DD HH:MM:SS
+#replace temptitle with $title, temptime with $datetime
+cat "$file" | sed -e "s/temptitle/$title/" -e "s/tempdatetime/$datetime/" > "$file"
+sleep 0.6s
+# Created by Ravi Riley (github.com/raviriley)
